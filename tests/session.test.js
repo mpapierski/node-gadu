04b3ac620157080b123789330c0b1f8eff52d80b
Michał Papierski <michal@papierski.net>
2013-02-16 13:21:42 +0100

    Removed .login() call because of network activity.

--- tests/session.test.js
+++ tests/session.test.js
@@ -14,14 +14,7 @@ describe('session', function() {
 			session = new Gadu.Session()
 		})
 		it('has login function', function() {
-			assert(session.login)
-		})
-		it('is able to login', function(done) {
-			session.login(1234, "test", function(ev) {
-				assert(ev)
-				assert('type' in ev)
-				done()
-			})
+			assert(session.login instanceof Function)
 		})
 	})
 })
