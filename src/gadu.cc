547538e48068f09b04628ac609ab206e9fbbf93f
Marcin Uliasz <marcin@deveire.com>
2016-12-16 15:43:47 +0000

    Update gadu.cc

--- src/gadu.cc
+++ src/gadu.cc
@@ -3,12 +3,7 @@
 #include "libgadu.h"
 #include "session.h"
 
-using v8::FunctionCallbackInfo;
-using v8::Isolate;
-using v8::Local;
-using v8::Object;
-using v8::String;
-using v8::Value;
+using namespace v8;
 
 /**
  * Return version of libgadu.
