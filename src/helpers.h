a2410fcb80b7aa1925000a2b3adfefd4cc69985e
Marcin Uliasz <marcin@deveire.com>
2016-12-20 17:33:53 +0000

    Update helpers.h

--- src/helpers.h
+++ src/helpers.h
@@ -33,7 +33,6 @@ public:
 #define NODE_SET_ATTRIBUTE(isolate, target, key, value) \
 	do { \
 		unsigned int mask = v8::ReadOnly | v8::DontDelete; \
-		target->Set(String::NewFromUtf8(isolate, key), (value), static_cast<v8::PropertyAttribute>(mask)); \
+		target->Set(String::NewFromUtf8(isolate, key), value); \
 	} while(0)
-
 #endif
